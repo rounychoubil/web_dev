c8cffc6b13856fff24eb668cfc2fc65f993d2245
rounychoubil <crouny15@gmail.com>
2023-05-24 15:55:36 -0400

    learn heading image and paragraphe html tags

--- core/urls.py
+++ core/urls.py
@@ -20,5 +20,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", TemplateView.as_view(template_name='index.html'), name="")
+    path("", TemplateView.as_view(template_name='index.html'), name="home"),
+    path('landing/',TemplateView.as_view(template_name='landing.html'),name="landing"),
 ]
